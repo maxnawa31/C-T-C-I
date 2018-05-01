bd95a432375366c4ae27ec2000efc00831860872
maxnawa31 <mnawa31@gmail.com>
2018-04-30 22:21:27 -0700

    finished memoization

--- algos.js
+++ algos.js
@@ -5,3 +5,16 @@ function fibonacci(n) {
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+//Fibonacci memoization
+
+function fibonacci(n, memo = []) {
+  if (n < 2) {
+    return n;
+  } else if (memo[n]) {
+    return memo[n];
+  } else {
+    memo[n] = fibonacci(n - 2, memo) + fibonacci(n - 1, memo);
+    return memo[n];
+  }
+}
