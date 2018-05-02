d4c3f8ae1e1af5f8b3b9011452002620314b45af
maxnawa31 <mnawa31@gmail.com>
2018-05-01 18:33:47 -0700

    completed stairs combination

--- algos.py
+++ algos.py
@@ -3,3 +3,11 @@ def fibonacci(n):
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+#stairs combination
+def davis(n, memo={1: 1, 2: 3, 3: 4}):
+    if n in memo:
+        return memo[n]
+    memo[n] = davis(n - 1, memo) + davis(n - 2, memo) + davis(n - 3, memo)
+    return memo[n]
