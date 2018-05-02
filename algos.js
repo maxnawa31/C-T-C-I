d6a15bebc015a66b230e9e315d220d33c269dedc
maxnawa31 <mnawa31@gmail.com>
2018-05-01 18:37:41 -0700

    finished javascript stairs

--- algos.js
+++ algos.js
@@ -18,3 +18,13 @@ function fibonacci(n, memo = []) {
     return memo[n];
   }
 }
+
+//stairs combination
+
+function davis(n, memo = { 1: 1, 2: 3, 3: 4 }) {
+  if (n in memo) {
+    return memo[n];
+  }
+  memo[n] = davis(n - 1, memo) + davis(n - 2, memo) + davis(n - 3, memo);
+  return memo[n];
+}
